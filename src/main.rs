040885c15f8450c3d11c0ce88af0abde920643e9
Levente Polyak <anthraxx@archlinux.org>
2020-05-20 19:29:57 +0200

    improve concurrency by collecting and joining all futures

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,13 @@ use anyhow::{Result, bail};
 
 use std::env;
 use std::vec::Vec;
-use keycloak::types::GroupRepresentation;
-use std::borrow::Cow;
+use keycloak::types::{UserRepresentation, CredentialRepresentation};
+use futures::future::try_join_all;
+
+async fn get_user_credentials<'a>(admin: &'a KeycloakAdmin<'a>, realm: &str, member: UserRepresentation<'a>) -> Result<(UserRepresentation<'a>, Vec<CredentialRepresentation<'a>>)> {
+    let credentials = admin.users_credentials_get(realm, member.id.as_ref().unwrap().as_ref()).await?;
+    Ok((member, credentials))
+}
 
 // TODO: error handling for all unwrap shizzle
 async fn run() -> Result<()> {
@@ -28,19 +33,6 @@ async fn run() -> Result<()> {
 
     let admin = KeycloakAdmin::new(url, admin_token, client);
 
-    let users = admin
-        .users_get(realm, None, None, None, None, None, None, None, None)
-        .await?;
-
-    for user in users {
-        // println!("user: {:?}", user);
-    }
-
-    let actions = admin.authentication_required_actions_get(realm).await?;
-    for action in actions {
-        // println!("action: {:?}", action);
-    }
-
     let groups_for_2fa = vec!["Arch Linux Staff"];
 
     let groups = admin.groups_get(realm, None, None, None, None).await?;
@@ -48,53 +40,56 @@ async fn run() -> Result<()> {
         groups_for_2fa.contains(&group.name.as_ref().unwrap().as_ref())
     }).collect::<Vec<_>>();
 
-    for group in groups {
+
+    let groups_members = groups.into_iter().flat_map(|group| {
         println!("group: {:?}", group);
 
-        let sub_groups = group.sub_groups.as_ref().unwrap();
-        for sub_group in sub_groups {
+        group.sub_groups.as_ref().unwrap().iter().map(|sub_group| {
             println!("-> sub group: {:?}", sub_group);
+            Box::pin(admin.groups_members_get(realm, sub_group.id.as_ref().unwrap(), None, None, None))
+        })
+    });
 
-            // TODO: flatten users and remove duplicates who are in multiple groups
-            let members = admin.groups_members_get(realm, sub_group.id.as_ref().unwrap(), None, None, None).await?;
-            for member in members {
-                println!("-> member: {:?}", member.username.unwrap());
-                //println!("-> member: {:?}", member.username.unwrap());
-                //println!("   required actions: {:?}", member.required_actions);
-
-                // Skip all users that already have a require action to configure TOTP
-                if let Some(required_actions) = member.required_actions {
-                    if required_actions.contains(&"CONFIGURE_TOTP".into()) {
-                        continue;
-                    }
-                }
-
-                let credentials = admin.users_credentials_get(realm, member.id.as_ref().unwrap().as_ref()).await?;
-                println!("-> credentials: {:?}", credentials);
-
-                // TODO: if user has totp in credentials: [] -> skip
-                if credentials.iter().any(|credential| credential.type_.as_ref().map(|type_| type_.eq("otp")).unwrap_or(false)) {
-                    continue;
-                }
-
-                // add docs -> make a second loop and remove require user action for TOTP in case the credentials already have totp, this is required as get->check->put is not race condition free and a user can setup totp in between get->put
-
-                // to reduce window of opportunity, we do an additional get->set->put per user inside a lopp
-                let mut member = admin.user_get(realm, &member.id.as_ref().unwrap()).await?;
-                member.required_actions = match member.required_actions {
-                    None => Some(vec!["CONFIGURE_TOTP".into()]),
-                    Some(mut required_actions) => {
-                        let totp = "CONFIGURE_TOTP".into();
-                        if !required_actions.contains(&totp) {
-                            required_actions.push(totp);
-                        }
-                        Some(required_actions)
-                    },
-                };
-                println!("set required actions: {:?}", member.required_actions);
-                // TODO: put back user in non dry mode
-            }
+    // TODO: remove duplicates who are in multiple groups
+    let f = try_join_all(groups_members).await?;
+    let members = f.into_iter().flatten().filter(|member| {
+        // Skip all users that already have a require action to configure TOTP
+        if let Some(required_actions) = &member.required_actions {
+            return !required_actions.contains(&"CONFIGURE_TOTP".into());
         }
+        true
+    }).collect::<Vec<_>>();
+
+    println!("members: {}", members.len());
+
+    let users_credentials = try_join_all(members.into_iter().map(|member| get_user_credentials(&admin, realm, member))).await?;
+    for (member, credentials) in users_credentials {
+        println!("-> member: {:?}", member.username.unwrap());
+        //println!("-> member: {:?}", member.username.unwrap());
+        //println!("   required actions: {:?}", member.required_actions);
+
+        let has_otp = credentials.iter().any(|credential| credential.type_.as_ref().map(|type_| type_.eq("otp")).unwrap_or(false));
+        if has_otp {
+            continue;
+        }
+
+        println!("-> credentials: {:?}", credentials);
+
+        // add docs -> make a second loop and remove require user action for TOTP in case the credentials already have totp, this is required as get->check->put is not race condition free and a user can setup totp in between get->put
+        // to reduce window of opportunity, we do an additional get->set->put per user inside a lopp
+        let mut member = admin.user_get(realm, &member.id.as_ref().unwrap()).await?;
+        member.required_actions = match member.required_actions {
+            None => Some(vec!["CONFIGURE_TOTP".into()]),
+            Some(mut required_actions) => {
+                let totp = "CONFIGURE_TOTP".into();
+                if !required_actions.contains(&totp) {
+                    required_actions.push(totp);
+                }
+                Some(required_actions)
+            },
+        };
+        println!("set required actions: {:?}", member.required_actions);
+        // TODO: put back user in non dry mode
     }
 
     Ok(())
