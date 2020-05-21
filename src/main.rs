cf4c9f11ed4499800b99672b57c294a33ad72d8e
Levente Polyak <anthraxx@archlinux.org>
2020-05-21 13:49:05 +0200

    logging debug and info messages while processing

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,9 @@ use std::vec::Vec;
 use keycloak::types::{UserRepresentation, CredentialRepresentation};
 use futures::future::try_join_all;
 
+use log::{debug, info, error};
+use env_logger::Env;
+
 async fn get_user_credentials<'a>(admin: &'a KeycloakAdmin<'a>, realm: &str, member: UserRepresentation<'a>) -> Result<(UserRepresentation<'a>, Vec<CredentialRepresentation<'a>>)> {
     let credentials = admin.users_credentials_get(realm, member.id.as_ref().unwrap().as_ref()).await?;
     Ok((member, credentials))
@@ -29,6 +32,8 @@ async fn run() -> Result<()> {
     )?;
 
     let client = Client::new();
+
+    info!("acquire API token for keycloak {} using realm {}", url, realm);
     let admin_token = KeycloakAdminToken::acquire(url, username, password, &client).await?;
 
     let admin = KeycloakAdmin::new(url, admin_token, client);
@@ -40,12 +45,12 @@ async fn run() -> Result<()> {
         groups_for_2fa.contains(&group.name.as_ref().unwrap().as_ref())
     }).collect::<Vec<_>>();
 
-
     let groups_members = groups.into_iter().flat_map(|group| {
-        println!("group: {:?}", group);
+        let group_name = group.name.as_ref().unwrap().as_ref();
+        debug!("processing group: {}", group_name);
 
         group.sub_groups.as_ref().unwrap().iter().map(|sub_group| {
-            println!("-> sub group: {:?}", sub_group);
+            info!("collecting members of sub group: {}", sub_group.name.as_ref().unwrap());
             Box::pin(admin.groups_members_get(realm, sub_group.id.as_ref().unwrap(), None, None, None))
         })
     });
@@ -53,27 +58,35 @@ async fn run() -> Result<()> {
     // TODO: remove duplicates who are in multiple groups
     let f = try_join_all(groups_members).await?;
     let members = f.into_iter().flatten().filter(|member| {
+        let username = member.username.as_ref().unwrap();
         // Skip all users that already have a require action to configure TOTP
         if let Some(required_actions) = &member.required_actions {
-            return !required_actions.contains(&"CONFIGURE_TOTP".into());
+            if required_actions.contains(&"CONFIGURE_TOTP".into()) {
+                debug!("CONFIGURE_TOTP present in required actions, skipping user: {}", username);
+                return false;
+            }
         }
+        debug!("CONFIGURE_TOTP not present in required actions, proceeding with user: {}", username);
         true
     }).collect::<Vec<_>>();
 
-    println!("members: {}", members.len());
+    info!("collected {} users whose credentials need to be checked", members.len());
 
     let users_credentials = try_join_all(members.into_iter().map(|member| get_user_credentials(&admin, realm, member))).await?;
     for (member, credentials) in users_credentials {
-        println!("-> member: {:?}", member.username.unwrap());
-        //println!("-> member: {:?}", member.username.unwrap());
-        //println!("   required actions: {:?}", member.required_actions);
+        let username = member.username.as_ref().unwrap();
+        let credential_types = credentials.iter().map(|credential| credential.type_.as_ref().unwrap().as_ref()).collect::<Vec<_>>();
+        let required_actions = member.required_actions.as_ref().map(|actions| actions.into_iter().map(|s| s.as_ref()).collect::<Vec<_>>()).unwrap_or(vec![].into());
 
-        let has_otp = credentials.iter().any(|credential| credential.type_.as_ref().map(|type_| type_.eq("otp")).unwrap_or(false));
+        debug!("user {} configured credentials: {:?}, required_actions: {:?}", username, credential_types, required_actions);
+
+        let has_otp = credentials.into_iter().any(|credential| credential.type_.as_ref().map(|type_| type_.eq("otp")).unwrap_or(false));
         if has_otp {
+            debug!("otp present in credentials, skipping user: {}", username);
             continue;
         }
 
-        println!("-> credentials: {:?}", credentials);
+        info!("enforce required action CONFIGURE_TOTP for user: {}", username);
 
         // add docs -> make a second loop and remove require user action for TOTP in case the credentials already have totp, this is required as get->check->put is not race condition free and a user can setup totp in between get->put
         // to reduce window of opportunity, we do an additional get->set->put per user inside a lopp
@@ -88,7 +101,6 @@ async fn run() -> Result<()> {
                 Some(required_actions)
             },
         };
-        println!("set required actions: {:?}", member.required_actions);
         // TODO: put back user in non dry mode
     }
 
@@ -97,11 +109,13 @@ async fn run() -> Result<()> {
 
 #[tokio::main]
 async fn main() {
-    println!("Starting gluebuddy...");
+    env_logger::init_from_env(Env::default()
+        .default_filter_or("info"));
+
     if let Err(err) = run().await {
-        eprintln!("Error: {:?}", err);
+        error!("Error: {:?}", err);
         for cause in err.chain() {
-            eprintln!("Because: {:?}", cause)
+            error!("Caused by: {:?}", cause)
         }
         std::process::exit(1)
     }
