ed2ef198c3953ee6716ae4341ed1abd91c10c13e
Christian Heusel <christian@heusel.eu>
2023-11-25 18:47:13 +0100

    feat(gitlab): enable packaging issues and MRs publicly
    related to https://gitlab.archlinux.org/archlinux/infrastructure/-/issues/522
    fixes https://gitlab.archlinux.org/archlinux/ideas/-/issues/5

--- src/components/gitlab/core.rs
+++ src/components/gitlab/core.rs
@@ -1036,8 +1036,8 @@ impl GitLabGlue {
         project: &GroupProjects,
     ) -> Result<bool> {
         let expected_request_access_enabled = false;
-        let expected_issues_access_level = ProjectFeatureAccessLevel::Private;
-        let expected_merge_requests_access_level = ProjectFeatureAccessLevel::Private;
+        let expected_issues_access_level = ProjectFeatureAccessLevel::Enabled;
+        let expected_merge_requests_access_level = ProjectFeatureAccessLevel::Enabled;
         let expected_merge_method = ProjectMergeMethod::FastForward;
         let expected_only_allow_merge_if_all_discussions_are_resolved = true;
         let expected_builds_access_level = ProjectFeatureAccessLevel::Disabled;
